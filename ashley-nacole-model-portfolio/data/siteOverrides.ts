6d0f50a61c1a45dfa81e42cb65008529844eaf09
ashdnaiel-sys <ashdnaiel@gmail.com>
2026-08-31 14:40:26 -0500

    Update contact email and selected portfolio choices

---
+++ ashley-nacole-model-portfolio/data/siteOverrides.ts
@@ -0,0 +1,13 @@
+export const siteOverrides = {
+  contactEmail: 'ashleynacole011@gmail.com',
+  selectedPortfolioIds: [
+    'beauty-direct-clean-close',
+    'fashion-black-bodysuit-seated',
+    'lifestyle-leopard-chair',
+    'fashion-accessory-close',
+    'swim-leaf-standing',
+    'fitness-studio-seated-sneakers',
+    'fashion-bw-ribbed-motion',
+    'lifestyle-cream-brick-alley',
+  ],
+} as const;
